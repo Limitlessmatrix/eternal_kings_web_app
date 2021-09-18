d77c22030db46bdc2bb143c13b08be12ffc6387f
Development <limitbreakerofchains@protonmail.com>
2021-09-18 14:06:40 -0700

    first

---
+++ src/Crown.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GiCrown } from "react-icons/gi";
+
+class Crown extends React.Component{
+  render(){
+    return <h1> <GiCrown /> </h1>
+  }
+}
+
+
+export default Crown;
